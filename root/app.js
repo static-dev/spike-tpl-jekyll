0b61ca78bae1e2c09fe0f98be211e8b037ad2e55
Covington Doan <covington@covingtondoan.com>
2017-03-08 15:36:35 -0600

    update for spike 0.15.0

--- root/app.js
+++ root/app.js
@@ -1,8 +1,6 @@
-const path = require('path')
-const HardSourcePlugin = require('hard-source-webpack-plugin')
 const htmlStandards = require('reshape-standard')
 const cssStandards = require('spike-css-standards')
-const jsStandards = require('babel-preset-latest')
+const jsStandards = require('babel-preset-env')
 const pageId = require('spike-page-id')
 const Collections = require('spike-collections')
 
@@ -14,9 +12,9 @@ const collections = new Collections({
       files: 'posts/**',
       permalinks: Collections.jekyll.date,
       paginate: {
-        perPage: 3,
+        perPage: 2,
         template: 'views/_page_template.sgr',
-        output: (n) => `posts/page${n}.html`
+        output: (n) => `page${n}.html`
       }
     }
   }
@@ -28,23 +26,11 @@ module.exports = {
     html: '*(**/)*.sgr',
     css: '*(**/)*.sss'
   },
-  ignore: ['**/layout.sgr', '**/_*', '**/.*', '_cache/**', 'readme.md'],
-  reshape: (ctx) => {
-    return htmlStandards({
-      webpack: ctx,
-      locals: collections.locals(ctx, Object.assign({ pageId: pageId(ctx) }, locals))
-    })
-  },
-  postcss: (ctx) => {
-    return cssStandards({ webpack: ctx })
-  },
-  babel: { presets: [jsStandards] },
-  plugins: [
-    collections,
-    new HardSourcePlugin({
-      environmentPaths: { root: __dirname },
-      recordsPath: path.join(__dirname, '_cache/records.json'),
-      cacheDirectory: path.join(__dirname, '_cache/hard_source_cache')
-    })
-  ]
+  ignore: ['**/layout.sgr', '**/_*', '**/.*', '_cache/**', 'readme.md', 'yarn.lock'],
+  reshape: htmlStandards({
+    locals: (ctx) => collections.locals(ctx, locals)
+  }),
+  postcss: cssStandards(),
+  babel: { presets: [[jsStandards, { modules: false }]] },
+  plugins: [collections]
 }
